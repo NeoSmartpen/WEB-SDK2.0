357c55b4b92d5d68951c3d5a986da2760800b09b
Wonho-Neo <swh1182@neolab.net>
2026-01-23 11:27:25 +0900

    types: re-export public types from root entry

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,17 @@ import PenMessageType, {SettingType} from './API/PenMessageType'
 
 // export default PenController
 export {PenController, Dot, NoteServer, PenHelper, PenMessageType, SettingType,}
+
+export type {
+  PageInfo,
+  PageInfo2,
+  PaperSize,
+  PaperBase,
+  ScreenDot,
+  View,
+  Options,
+  VersionInfo,
+  SettingInfo,
+  DotErrorInfo,
+  Paper,
+} from './Util/type'
